4eca8afa20ff435482e319782f5e5ec219546d19
Shubham <shubhamzxv@gmail.com>
2024-05-10 01:02:14 +0530

    changed backend Link

--- src/config.js
+++ src/config.js
@@ -1,1 +1,4 @@
-export const API_BASE_URL = "http://localhost:4000";
+// export const API_BASE_URL = "http://localhost:4000";
+
+// Live Backend Link
+export const API_BASE_URL = "https://backend-x.onrender.com";
